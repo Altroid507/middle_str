e2d2abffade7323d662194d5e65d7f7c48e2be0e
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-12-04 11:10:53 +0300

    Add files via upload

---
+++ middle_str.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include "middle_str.h"
+
+using namespace std;
+
+long long itc_len(string str) {		//Собственная функция
+	long long size;
+	size = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		size++;
+	}
+	return size;
+}
+
+bool itc_isDigit(unsigned char c) {
+	if (c >= '0' && c <= '9') return true;
+	return false;
+}
+
+unsigned char itc_toUpper(unsigned char c) {
+	if (c >= 97 && c <= 122) return c -= 32;
+	return c;
+}
+
+unsigned char itc_toLower(unsigned char c) {	//Собственная функция
+	if (c >= 65 && c <= 90) return c += 32;
+	return c;
+}
+
+unsigned char itc_changeCase(unsigned char c) {
+	if (c >= 97 && c <= 122) return itc_toUpper(c);
+	if (c >= 65 && c <= 90) return itc_toLower(c);
+	return c;
+}
+
+bool itc_compare(string s1, string s2) {
+	int check = 0;
+	if (itc_len(s1) != itc_len(s2)) return false;
+	for (int i = 0; i <= itc_len(s1); i++) {
+		if (s1[i] != s2[i]) check = 1;
+	}
+	if (check == 0) return true;
+	return false;
+}
+
+int itc_countWords(string str) {
+	int cntr = 0;
+	int j = 0;
+	str += " ";
+	for (int i = 0; i < itc_len(str); i++) {
+		if (str[i] == ' ') {
+			cntr++;
+			for (; j <= i; j++) {
+				if ((str[j] >= '!' && str[j] <= '@') || (str[j] >= '[' && str[j] <= '`') || (str[j] >= '{' && str[j] <= '~')) {
+					cntr--;
+				}
+			}
+		}
+	}
+	return cntr;
+}
+
+int main() {
+	cout << itc_countWords("I like computer scince") << endl;
+	cout << itc_countWords("he#he") << endl;
+}
---
+++ middle_str.h
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+long long itc_len(string str);
+bool itc_isDigit(unsigned char c);
+unsigned char itc_toUpper(unsigned char c);
+unsigned char itc_toLower(unsigned char c);
+unsigned char itc_changeCase(unsigned char c);
+bool itc_compare(string s1, string s2);
+int itc_countWords(string str);
+
+#ifndef MIDDLESTRING_MIDDLE_STR_H
+#define MIDDLESTRING_MIDDLE_STR_H
+
+#endif
