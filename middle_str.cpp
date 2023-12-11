c60b90deb36bf2d917d11a76d8f5fa22b8e38ad0
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-12-11 11:27:21 +0300

    Add files via upload

--- middle_str.cpp
+++ middle_str.cpp
@@ -13,6 +13,22 @@ long long itc_len(string str) {		//Собственная функция
 	return size;
 }
 
+string itc_slice_str(string str, int start, int end) {		//Собственная функция
+	string res;
+	if (start > end) return str;
+	if (end < itc_len(str)) {
+		for (int i = (start - 1); i <= (end - 1); i++) {
+			res += str[i];
+		}
+	}
+	else {
+		for (int i = (start - 1); i <= itc_len(str); i++) {
+			res += str[i];
+		}
+	}
+	return res;
+}
+
 bool itc_isDigit(unsigned char c) {
 	if (c >= '0' && c <= '9') return true;
 	return false;
@@ -61,7 +77,31 @@ int itc_countWords(string str) {
 	return cntr;
 }
 
+string itc_maxCharWord(string str) {
+	int space1 = 0, space2;
+	string maxim = "";
+	str = " " + str + " ";
+	for (int i = 1; i <= itc_len(str); i++) {
+		if (str[i] == ' ') {
+			space2 = i;
+			if (itc_len(maxim) < itc_len(itc_slice_str(str, space1 + 2, space2))) {
+				maxim = itc_slice_str(str, space1 + 2, space2);
+				for (int j = space1; j <= space2; j++) {
+					if ((str[j] >= '!' && str[j] <= '@') || (str[j] >= '[' && str[j] <= '`') || (str[j] >= '{' && str[j] <= '~')) {
+						maxim = "";
+					}
+				}
+			}
+			space1 = i;
+		}
+	}
+	if (maxim == "") maxim = "error";
+	return maxim;
+}
+
 int main() {
-	cout << itc_countWords("I like computer scince") << endl;
-	cout << itc_countWords("he#he") << endl;
+	cout << itc_maxCharWord("one two four") << endl;
+	cout << itc_maxCharWord("one four nine") << endl;
+	cout << itc_maxCharWord("one, four, nine.") << endl;
+	cout << itc_maxCharWord("onefournine") << endl;
 }
--- middle_str.h
+++ middle_str.h
@@ -1,6 +1,7 @@
 #include <string>
 using namespace std;
 
+string itc_slice_str(string str, int start, int end);
 long long itc_len(string str);
 bool itc_isDigit(unsigned char c);
 unsigned char itc_toUpper(unsigned char c);
@@ -8,6 +9,7 @@ unsigned char itc_toLower(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
 bool itc_compare(string s1, string s2);
 int itc_countWords(string str);
+string itc_maxCharWord(string str);
 
 #ifndef MIDDLESTRING_MIDDLE_STR_H
 #define MIDDLESTRING_MIDDLE_STR_H
