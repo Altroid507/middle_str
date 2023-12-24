f027a62f3cf68fda169e6243a2312b6b29e35da7
Altroid507 <125668171+Altroid507@users.noreply.github.com>
2023-12-24 21:38:43 +0300

    Update middle_str(3).cpp

--- middle_str(3).cpp
+++ middle_str(3).cpp
@@ -22,3 +22,33 @@ bool itc_isIp(string str) {
 	if (number < 0 || number > 255 || kolvo > 3) return false;
 	return true;
 }
+
+string itc_DecToBin(string str) {
+	string res = "", res1 = "";
+	bool chk = false;
+	long long number = 0;
+	for (int i = 0; i < itc_len(str); i++) {
+		if (!itc_isDigit(str[i])) {
+			res += str[i];
+		}
+		else if (itc_isDigit(str[i])) {
+			if (str[i] == '0') {
+				res += '0';
+			}
+			int j = i;
+			while (itc_isDigit(str[j])) {
+				number = number * 10 + (str[j] - '0');
+				j++;
+			}
+			i = j - 1;
+			number = itc_changeNum(number, 2);
+			while (number != 0) {
+				res1 += (number % 10) + '0';
+				number /= 10;
+			}
+			res += itc_reverse_str(res1);
+			res1 = "";
+		}
+	}
+	return res;
+}
